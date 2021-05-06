04c798b853942a4bfe320f1178b64c7737c8c8d3
jesusrubio3 <jesusrubiomartin3@gmail.com>
2021-05-06 14:14:56 +0200

    segunda página

--- app.py
+++ app.py
@@ -9,4 +9,18 @@ def inicio():
   
     return render_template("index.html",lista=lista)
 
+@app.route('/libro/<isbn>')
+def libro(isbn):
+    with open("books.json") as libros:
+        todos_isbn=[]
+        lista=json.load(libros)
+        for i in lista:
+            isbn_libro=i.get("isbn")
+            todos_isbn.append(isbn_libro)
+        if isbn not in todos_isbn:
+            abort(404)
+        
+        
+    return render_template("hola.html",lista=lista,isbn=isbn)
+
 app.run(debug=True)
