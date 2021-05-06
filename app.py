ce56ed0c7652b10fe63751568737c2e3a0850f4d
jesusrubio3 <jesusrubiomartin3@gmail.com>
2021-05-06 14:01:18 +0200

    primera página

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, abort
+import json
 app=Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def inicio():
-    f=open("books.json")
-    lista=f.read()
-    f.close()
-
+    with open("books.json") as libros:
+        lista=json.load(libros)
+  
     return render_template("index.html",lista=lista)
 
 app.run(debug=True)
